4d862ec6b7d922f8ca6ffd2a0a9e833a39eae747
v-cd <vishva@crustdata.co>
2025-01-08 16:18:11 +0530

    Fix install script

--- get.sh
+++ get.sh
@@ -3,6 +3,6 @@
 REPO_ID=v-cd/slack-nativefier
 
 sudo mkdir -p /opt/slack
-sudo wget raw.githubusercontent.com/$REPO_ID/app.asar -O /opt/slack/app.asar
-sudo wget raw.githubusercontent.com/$REPO_ID/slack -O /usr/bin/slack
-sudo wget raw.githubusercontent.com/$REPO_ID/slack.desktop -O /usr/share/applications/slack.desktop
+sudo wget raw.githubusercontent.com/$REPO_ID/refs/heads/master/app.asar -O /opt/slack/app.asar
+sudo wget raw.githubusercontent.com/$REPO_ID/refs/heads/master/slack -O /usr/bin/slack
+sudo wget raw.githubusercontent.com/$REPO_ID/refs/heads/master/slack.desktop -O /usr/share/applications/slack.desktop
